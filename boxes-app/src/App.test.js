b37d2932e81edad62ea2b7587dfe6a02134f0804
marauder77 <63757565+marauder77@users.noreply.github.com>
2023-05-16 19:31:13 -0700

    adding react form files

---
+++ boxes-app/src/App.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+it("renders without crashing", function() {
+  render(<App />);
+});
+
+it("matches snapshot", function() {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
